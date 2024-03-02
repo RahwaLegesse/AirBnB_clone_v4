09918b33fc4cd41804ba9ec54e20a861fc92c155
RahwaLegesse <rahwalegesse31@gmail.com>
2024-03-02 11:10:00 +0000

    task1

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-""" Starts a Flash Web Application """
+tarts a Flash Web Application """
 from models import storage
 from models.state import State
 from models.city import City
@@ -7,9 +6,8 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+import uuid
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
@@ -18,7 +16,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -30,14 +28,15 @@ def hbnb():
 
     amenities = storage.all(Amenity).values()
     amenities = sorted(amenities, key=lambda k: k.name)
-
+    cache_id = str(uuid.uuid4())
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
